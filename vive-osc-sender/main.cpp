e4fa6401a4a351b033e5aa446c0f29ff0348c94c
hglmz <hglmz@HALIT_WORKSTATI>
2023-03-23 06:36:30 +0300

    çalışıyor gibi

--- vive-osc-sender/main.cpp
+++ vive-osc-sender/main.cpp
@@ -1,77 +1,150 @@
-//
-// HTC Vive Lighthouse Tracking Example
-// By Peter Thor 2016, 2017, 2018, 2019
-//
-
-#include "stdafx.h"
-#include "Windows.h"
-#include <string>
-#include <conio.h>
-#include <iomanip>		// for std::setprecision
 #include <iostream>
+#include <thread>
+#include <chrono>
+#include <string>
+#include <array>
+#include <memory>
+#include <cstdint>
+#include <algorithm>
+#include <iterator>
 
-#include "LighthouseTracking.h"
+#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
 
-// windows keyboard input
-#include <conio.h>
-#include <vector>
+#include <openvr.h>
 
-using std::vector;
-using std::string;
+constexpr uint8_t MESSAGE_IDENTIFIER = 0xD1;
+constexpr size_t MESSAGE_SIZE = 29;
 
-int _tmain(int argc, _TCHAR* argv[])
+std::array<uint8_t, MESSAGE_SIZE> createMessage(float pitch, float yaw, float roll, float x, float y, float z)
 {
-	int shouldListDevicesAndQuit = 0;
-	int port = 9999;
-	char ip_address[128];
-	sprintf_s(ip_address, sizeof(ip_address), "127.0.0.1");
+    std::array<uint8_t, MESSAGE_SIZE> message{};
+    message[0] = MESSAGE_IDENTIFIER;
+    message[1] = 0x00; // Camera ID
 
-	// very basic command line parser, from:
-	// http://stackoverflow.com/questions/17144037/change-a-command-line-argument-argv
-	vector<string> validArgs;
-	validArgs.reserve(argc); //Avoids reallocation; it's one or two (if --item is given) too much, but safe and not pedentatic while handling rare cases where argc can be zero
+    auto floatToBytes = [](float value) {
+        uint32_t intValue = *reinterpret_cast<uint32_t*>(&value);
+        return std::array<uint8_t, 4>{static_cast<uint8_t>(intValue),
+            static_cast<uint8_t>(intValue >> 8),
+            static_cast<uint8_t>(intValue >> 16),
+            static_cast<uint8_t>(intValue >> 24)};
+    };
 
-	// parse all user input
-	for (int i = 1; i<argc; ++i) {
-		const std::string myArg(argv[i]);
+    auto pitchBytes = floatToBytes(pitch);
+    auto yawBytes = floatToBytes(yaw);
+    auto rollBytes = floatToBytes(roll);
+    auto xBytes = floatToBytes(x);
+    auto yBytes = floatToBytes(y);
+    auto zBytes = floatToBytes(z);
 
-		if (myArg == std::string("--listdevices")) shouldListDevicesAndQuit = atoi(argv[i + 1]);
-		if (myArg == std::string("--ip")) sprintf_s(ip_address, sizeof(ip_address), argv[i + 1]);
-		if (myArg == std::string("--port")) port = atoi(argv[i + 1]);
+    std::copy(pitchBytes.begin(), pitchBytes.end(), message.begin() + 2);
+    std::copy(yawBytes.begin(), yawBytes.end(), message.begin() + 6);
+    std::copy(rollBytes.begin(), rollBytes.end(), message.begin() + 10);
+    std::copy(zBytes.begin(), zBytes.end(), message.begin() + 14);
+    std::copy(yBytes.begin(), yBytes.end(), message.begin() + 18);
+    std::copy(xBytes.begin(), xBytes.end(), message.begin() + 22);
 
-		validArgs.push_back(myArg);
-	}
+    // Zoom and focus not used
+    message[26] = 0x00;
+    message[27] = 0x00;
+    message[28] = 0x00;
 
-	// Create a new LighthouseTracking instance and parse as needed
-	LighthouseTracking *lighthouseTracking = new LighthouseTracking(IpEndpointName(ip_address, port));
-	if (lighthouseTracking) {
+    uint8_t checksum = 0;
+    for (size_t i = 0; i < MESSAGE_SIZE - 1; ++i)
+    {
+        checksum += message[i];
+    }
+    message[28] = checksum;
 
-		lighthouseTracking->PrintDevices();
-
-		if (!shouldListDevicesAndQuit) {
-			printf_s("Press 'q' to quit. Starting capture of tracking data...\n");
-
-			// This is our main loop run
-			while (lighthouseTracking->RunProcedure()) {
-
-				// Windows quit routine - adapt as you need
-				if (_kbhit()) {
-					char ch = _getch();
-					if ('q' == ch) {
-						printf_s("User pressed 'q' - exiting...");
-						break;
-					} else if ('l' == ch) {
-						lighthouseTracking->PrintDevices();
-					}
-				}
-
-				// a delay to not overheat your computer... :)
-				Sleep(2);
-			}
-		}
-
-		delete lighthouseTracking;
-	}
-	return EXIT_SUCCESS;
+    return message;
 }
 
+int main()
+{
+    const std::string ipAddress = "127.0.0.1";
+    const uint16_t port = 12345;
+
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+    {
+        std::cerr << "WSAStartup failed" << std::endl;
+        return 1;
+    }
+
+    SOCKET sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    if (sock == INVALID_SOCKET)
+    {
+        std::cerr << "Socket creation failed" << std::endl;
+
+        WSACleanup();
+        return 1;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = inet_addr(ipAddress.c_str());
+
+    // Initialize OpenVR
+    vr::EVRInitError initError = vr::VRInitError_None;
+    if (!vr::VR_IsRuntimeInstalled() || !vr::VR_IsHmdPresent())
+    {
+        std::cerr << "No VR headset found" << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    vr::IVRSystem* vrSystem = vr::VR_Init(&initError, vr::VRApplication_Background);
+    if (initError != vr::VRInitError_None)
+    {
+        std::cerr << "Failed to initialize VR runtime: " << vr::VR_GetVRInitErrorAsEnglishDescription(initError) << std::endl;
+        closesocket(sock);
+        WSACleanup();
+        return 1;
+    }
+
+    while (true)
+    {
+        vr::VRControllerState_t controllerState;
+        vr::TrackedDevicePose_t trackedDevicePose;
+        vr::ETrackedDeviceClass trackedDeviceClass;
+
+        for (vr::TrackedDeviceIndex_t deviceIndex = 0; deviceIndex < vr::k_unMaxTrackedDeviceCount; ++deviceIndex)
+        {
+            trackedDeviceClass = vrSystem->GetTrackedDeviceClass(deviceIndex);
+            if (trackedDeviceClass == vr::ETrackedDeviceClass::TrackedDeviceClass_GenericTracker)
+            {
+                vrSystem->GetControllerStateWithPose(vr::TrackingUniverseStanding, deviceIndex, &controllerState, sizeof(controllerState), &trackedDevicePose);
+                if (trackedDevicePose.bPoseIsValid)
+                {
+                    vr::HmdMatrix34_t pose = trackedDevicePose.mDeviceToAbsoluteTracking;
+                    float pitch = atan2f(pose.m[2][0], pose.m[2][2]);
+                    float yaw = atan2f(pose.m[0][2], pose.m[2][2]);
+                    float roll = atan2f(pose.m[1][0], pose.m[1][1]);
+
+                    float x = pose.m[0][3];
+                    float y = pose.m[1][3];
+                    float z = pose.m[2][3];
+
+                    auto message = createMessage(pitch, yaw, roll, x, y, z);
+                    sendto(sock, reinterpret_cast<char*>(message.data()), message.size(), 0, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress));
+                }
+            }
+        }
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    vr::VR_Shutdown();
+    closesocket(sock);
+    WSACleanup();
+
+    //return 0;
+   // std::cout << "Çýkmak için bir tuþa basýn...\n";
+   // std::cin.get();
+    //return 0;
+
+    system("pause");
+    return 0;
+}
