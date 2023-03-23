cfcd5be75d947cd9d5f959e89ec93f9720db9774
HALİT GÜLMEZ <halitgulmez@stu.aydin.edu.tr>
2023-03-23 14:55:46 +0300

    its working but little bugy

--- vive-osc-sender/main.cpp
+++ vive-osc-sender/main.cpp
@@ -168,7 +168,7 @@ int main()
             }
         }
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
     }
 
     vr::VR_Shutdown();
